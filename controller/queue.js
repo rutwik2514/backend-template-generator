5ac2084377601261078509ddb612561e77c37de9
unknown <rutwik2514@gmail.com>
2024-06-19 16:58:15 +0530

    connected to worker_service

--- controller/queue.js
+++ controller/queue.js
@@ -2,8 +2,10 @@ const { Queue } = require('bullmq');
 const IORedis = require('ioredis');
 
 const connection = new IORedis({
+    host: 'redis',
+    port: 6379,
     maxRetriesPerRequest: null
-});
+  });
 
 const downloadQueue = new Queue('jobQueue', { connection });
 
